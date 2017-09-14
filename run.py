3a6fb2e874aee31c067fa8d0fd548492dd583550
向晚 <253507853@qq.com>
2017-09-14 15:12:36 +0800

    更新readme和其他架构

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/Xiangwan')
-def details():
-	return render_template('A10119.html')
+
+@app.route('/<string:student_number>/details')
+def details(student_number):
+	return render_template(str(student_number) + '.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
