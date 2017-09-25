2d89e3f0715d11093af94a1f79265839dd8f4910
Qingyaya <shashaxiaoge@163.com>
2017-09-25 19:47:27 +0800

    我的最终文件

--- run.py
+++ run.py
@@ -1,11 +1,73 @@
+#-*- coding:utf-8 -*-
+
 from flask import Flask
 from flask import render_template
+import json
 
 app = Flask(__name__)
 
-@app.route('/<string:student_number>/details')
-def details(student_number):
-	return render_template(str(student_number) + '.html')
+def read_json_file(filepath):
+	jsonfile=open(filepath,'r+')
+	jsontext=jsonfile.read()
+	data=json.loads(jsontext)
+	return data
+
+@app.route('/')
+def C4():
+	data=read_json_file('static/data/index.json')
+	return render_template('index.html',data=data)
+
+@app.route('/details/<student_number>')
+def look_details(student_number):
+	user_data={}
+	data=read_json_file('static/data/index.json')
+	for item in data:
+
+		if student_number == item['student_number']:
+			
+			user_data=item
+			break
+	print user_data
+	return render_template('details.html',data=user_data)
+
+@app.route('/details/<string:student_number>/news')
+def news(student_number):
+	user_data={}
+	data=read_json_file('static/data/index.json')
+	for item in data:
+
+		if student_number == item['student_number']:
+			
+			user_data=item
+			break
+	print user_data
+	return render_template('news.html',data=user_data)
+
+@app.route('/details/<student_number>/readnote')
+def readnote(student_number):
+	user_data={}
+	data=read_json_file('static/data/index.json')
+	for item in data:
+
+		if student_number == item['student_number']:
+			
+			user_data=item
+			break
+	print user_data
+	return render_template('readnote.html',data=user_data)
+
+@app.route('/details/<student_number>/trival')
+def trival(student_number):
+	user_data={}
+	data=read_json_file('static/data/index.json')
+	for item in data:
+
+		if student_number == item['student_number']:
+			
+			user_data=item
+			break
+	print user_data
+	return render_template('trival.html',data=user_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
